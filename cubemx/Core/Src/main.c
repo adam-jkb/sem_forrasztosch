6289e6ecaa5c43bb23a1dd530606296b8f4199cf
Adam Jakab <adam.jkb@gmail.com>
2024-05-13 16:23:16 +0200

    flip led

--- cubemx/Core/Src/main.c
+++ cubemx/Core/Src/main.c
@@ -1,20 +1,20 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2023 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+******************************************************************************
+* @file : main.c
+* @brief: Main program body
+******************************************************************************
+* @attention
+*
+* Copyright (c) 2023 STMicroelectronics.
+* All rights reserved.
+*
+* This software is licensed under terms that can be found in the LICENSE file
+* in the root directory of this software component.
+* If no LICENSE file comes with this software, it is provided AS-IS.
+*
+******************************************************************************
+*/
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
@@ -40,15 +40,15 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-#define ESCAPE_NORM    "\033[0m"
-#define ESCAPE_BLACK   "\033[30m"
-#define ESCAPE_RED     "\033[31m"
-#define ESCAPE_GREEN   "\033[32m"
-#define ESCAPE_YELLOW  "\033[33m"
-#define ESCAPE_BLUE    "\033[34m"
+#define ESCAPE_NORM "\033[0m"
+#define ESCAPE_BLACK "\033[30m"
+#define ESCAPE_RED "\033[31m"
+#define ESCAPE_GREEN "\033[32m"
+#define ESCAPE_YELLOW "\033[33m"
+#define ESCAPE_BLUE "\033[34m"
 #define ESCAPE_MAGENTA "\033[35m"
-#define ESCAPE_CYAN    "\033[36m"
-#define ESCAPE_WHITE   "\033[37m"
+#define ESCAPE_CYAN "\033[36m"
+#define ESCAPE_WHITE "\033[37m"
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -76,197 +76,200 @@ int __io_putchar(int ch) {
 
 
 void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim) {
+
+}
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
+	printf("%lu\n", htim->Instance->CNT);
 }
 
 void HAL_TIM_PWM_PulseFinishedCallback(TIM_HandleTypeDef *htim) {
-	//printf("%lu\n", htim->Instance->CNT);	
+	printf("%lu\n", htim->Instance->CNT);
 }
 
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
-int main(void)
-{
-  /* USER CODE BEGIN 1 */
-  /* USER CODE END 1 */
+* @brief The application entry point.
+* @retval int
+*/
+int main(void) {
+/* USER CODE BEGIN 1 */
+/* USER CODE END 1 */
 
-  /* MCU Configuration--------------------------------------------------------*/
+/* MCU Configuration--------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+	HAL_Init();
 
-  /* USER CODE BEGIN Init */
-  /* USER CODE END Init */
+/* USER CODE BEGIN Init */
+/* USER CODE END Init */
 
-  /* Configure the system clock */
-  SystemClock_Config();
+/* Configure the system clock */
+	SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
-  //causes hard fault
-  //HAL_ADCEx_Calibration_Start(&hadc1);
-  /* USER CODE END SysInit */
+/* USER CODE BEGIN SysInit */
+/* USER CODE END SysInit */
 
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_ADC1_Init();
-  MX_I2C1_Init();
-  MX_TIM2_Init();
-  MX_TIM3_Init();
+/* Initialize all configured peripherals */
+	MX_GPIO_Init();
+	MX_ADC1_Init();
+	MX_I2C1_Init();
+	MX_TIM2_Init();
+	MX_TIM3_Init();
 
-  /* Initialize interrupts */
-  MX_NVIC_Init();
-  /* USER CODE BEGIN 2 */
-  HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1);
-  HAL_TIM_Encoder_Start_IT(&htim3, TIM_CHANNEL_1);  
-  HAL_ADC_Start(&hadc1);
+/* Initialize interrupts */
+	MX_NVIC_Init();
+/* USER CODE BEGIN 2 */
+	// CH1 is dead so only CH2
+	__HAL_TIM_ENABLE_IT(&htim2, TIM_IT_UPDATE);
+	HAL_TIM_PWM_Start_IT(&htim2, TIM_CHANNEL_2);
+	HAL_TIM_Encoder_Start(&htim3, TIM_CHANNEL_1);
+	
+	HAL_ADCEx_Calibration_Start(&hadc1);
+	HAL_ADC_Start(&hadc1);
 
-  DISPLAY_Init();
-  /* USER CODE END 2 */
+	DISPLAY_Init();
+/* USER CODE END 2 */
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-  TIM2->CCR1 = 32000;
-  //volatile uint32_t delay;
-  int16_t enc_curr, enc_prev = TIM3->CNT;
-  uint32_t curr, start = HAL_GetTick();
-  uint32_t state_curr1, state_curr2, state_prev2 = LL_GPIO_IsInputPinSet(BTN_2_GPIO_Port, BTN_2_Pin), state_prev1 = LL_GPIO_IsInputPinSet(BTN_1_GPIO_Port, BTN_1_Pin);
-  uint32_t sum = 0;
-  int32_t enc_delta = 0;
-  while (1) {
-	 // for(delay = 0; delay < 100000; delay++);
+/* Infinite loop */
+/* USER CODE BEGIN WHILE */
+	TIM2->CCR2 = 32000;
+	//volatile uint32_t delay;
+	int16_t enc_curr, enc_prev = TIM3->CNT;
+	uint32_t curr, start = HAL_GetTick();
+	uint32_t state_curr2, state_prev2 = LL_GPIO_IsInputPinSet(BTN_2_GPIO_Port, BTN_2_Pin),
+		state_curr1, state_prev1 = LL_GPIO_IsInputPinSet(BTN_1_GPIO_Port, BTN_1_Pin);
+	uint32_t sum = 0;
+	int32_t enc_delta = 0;
+	while (1) {
+		//for(volatile uint32_t delay = 0; delay < 100000; delay++);
+		
+		//printf(ESCAPE_GREEN "tick%lu" ESCAPE_NORM "\n", TIM2->CNT);
 
-	  //handle enc
-	  enc_curr = TIM3->CNT;
-	  state_curr1 = LL_GPIO_IsInputPinSet(BTN_1_GPIO_Port, BTN_1_Pin);
-	  state_curr2 = LL_GPIO_IsInputPinSet(BTN_2_GPIO_Port, BTN_2_Pin);
-	  curr = HAL_GetTick();
-	  if (state_curr1 != state_prev1) {
-	  	state_prev1 = state_curr1;
-		if(!state_curr1) {++sum;}
-	  }
-	  if (state_curr2 != state_prev2) {
-	  	state_prev2 = state_curr2;
-		if(!state_curr2) {++sum;}
-	  }
-	  if (enc_curr != enc_prev) {
-		enc_delta = (int32_t)(enc_curr - enc_prev);
-		if(enc_curr % 4 == 0) {
-			printf("click%lu\n",enc_delta);
+		//handle enc
+		enc_curr = TIM3->CNT;
+		state_curr1 = LL_GPIO_IsInputPinSet(BTN_1_GPIO_Port, BTN_1_Pin);
+		state_curr2 = LL_GPIO_IsInputPinSet(BTN_2_GPIO_Port, BTN_2_Pin);
+		curr = HAL_GetTick();
+		if(state_curr1 != state_prev1) {
+			state_prev1 = state_curr1;
+			if(!state_curr1) {++sum;}
 		}
-	  	enc_prev = enc_curr;
-	  }
-	  if(curr == start+100) {
-		  start = curr;
-		  if(sum) {
-			  printf("sum = %lu, tim = %lu\n", sum, curr);
-			  sum = 0;
-		  }
+		if(state_curr2 != state_prev2) {
+			state_prev2 = state_curr2;
+			if(!state_curr2) {++sum;}
+		}
+		if(enc_curr != enc_prev) {
+			enc_delta = (int32_t)(enc_curr - enc_prev);
+			if(enc_curr % 4 == 0) {
+				printf("click%lu\n",enc_delta);
+			}
+			enc_prev = enc_curr;
+		}
+		if(curr == start+100) {
+			start = curr;
+			if(sum) {
+				printf("sum = %lu, tim = %lu\n", sum, curr);
+				sum = 0;
+			}
+		}
+/* USER CODE END WHILE */
+
+/* USER CODE BEGIN 3 */
 	}
-    /* USER CODE END WHILE */
 
-    /* USER CODE BEGIN 3 */
-  }
-
-  /* USER CODE END 3 */
+/* USER CODE END 3 */
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+* @brief System Clock Configuration
+* @retval None
+*/
 void SystemClock_Config(void)
 {
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+	RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+	RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL6;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
+/** Initializes the RCC Oscillators according to the specified parameters
+* in the RCC_OscInitTypeDef structure.
+*/
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+	RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+	RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+	RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL6;
+	if(HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+		Error_Handler();
+	}
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+/** Initializes the CPU, AHB and APB buses clocks
+*/
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+	|RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_ADC;
-  PeriphClkInit.AdcClockSelection = RCC_ADCPCLK2_DIV4;
-  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	if(HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK) {
+		Error_Handler();
+	}
+	PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_ADC;
+	PeriphClkInit.AdcClockSelection = RCC_ADCPCLK2_DIV4;
+	if(HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK) {
+		Error_Handler();
+	}
 }
 
 /**
-  * @brief NVIC Configuration.
-  * @retval None
-  */
+* @brief NVIC Configuration.
+* @retval None
+*/
 static void MX_NVIC_Init(void)
 {
-  /* TIM2_IRQn interrupt configuration */
-  HAL_NVIC_SetPriority(TIM2_IRQn, 1, 0);
-  HAL_NVIC_EnableIRQ(TIM2_IRQn);
-  /* TIM3_IRQn interrupt configuration */
-  HAL_NVIC_SetPriority(TIM3_IRQn, 2, 0);
-  HAL_NVIC_EnableIRQ(TIM3_IRQn);
-  /* EXTI15_10_IRQn interrupt configuration */
-  //NVIC_SetPriority(EXTI15_10_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(),2, 0));
-  //NVIC_EnableIRQ(EXTI15_10_IRQn);
+/* TIM2_IRQn interrupt configuration */
+	HAL_NVIC_SetPriority(TIM2_IRQn, 1, 0);
+	HAL_NVIC_EnableIRQ(TIM2_IRQn);
+/* TIM3_IRQn interrupt configuration */
+	HAL_NVIC_SetPriority(TIM3_IRQn, 2, 0);
+	HAL_NVIC_EnableIRQ(TIM3_IRQn);
+/* EXTI15_10_IRQn interrupt configuration */
+	//NVIC_SetPriority(EXTI15_10_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(),2, 0));
+	//NVIC_EnableIRQ(EXTI15_10_IRQn);
 }
 
 /* USER CODE BEGIN 4 */
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
-  /* USER CODE END Error_Handler_Debug */
+* @brief This function is executed in case of error occurrence.
+* @retval None
+*/
+void Error_Handler(void) {
+/* USER CODE BEGIN Error_Handler_Debug */
+/* User can add his own implementation to report the HAL error return state */
+	__disable_irq();
+	while (1) {
+	}
+/* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t *file, uint32_t line)
-{
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
+* @brief Reports the name of the source file and the source line number
+* where the assert_param error has occurred.
+* @paramfile: pointer to the source file name
+* @paramline: assert_param error line source number
+* @retval None
+*/
+void assert_failed(uint8_t *file, uint32_t line) {
+/* USER CODE BEGIN 6 */
+/* User can add his own implementation to report the file name and line number,
+ ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+/* USER CODE END 6 */
 }
-#endif /* USE_FULL_ASSERT */
+#endif/* USE_FULL_ASSERT */
